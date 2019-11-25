84cb8a71c1865126fca5f516577d7c4cc0acaa43
Richiricheh <richard.cocksedge@gmail.com>
2019-11-25 11:14:14 -0500

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php return array(
- 'id' => 'techno11:osticket-snipe-it-integrator', # notrans
- 'version' => '1.2',
- 'name' => 'osTicket + Snipe-IT Integrator',
+ 'id' => 'techno11&rich_c:osticket-snipe-it-integrator', # notrans
+ 'version' => '1.3',
+ 'name' => 'Snipe-IT Plugin for osTicket',
  'author' => 'soren@thezaisers.me',
- 'description' => 'Auto-Links Asset ID that are marked in Square Brackets ([])',
- 'url' => 'https://github.com/Techno11/os-ticket-snipe-it',
+ 'description' => 'An Integration between Snipe-IT & osTicket',
+ 'url' => 'https://github.com/Richiricheh/os-ticket-snipe-it',
  'plugin' => 'class.SnipeITIntegrator.php:SnipeITIntegrator'
 );
